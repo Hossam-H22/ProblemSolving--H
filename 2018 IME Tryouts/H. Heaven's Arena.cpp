38de171d4d1daaad03bb973a138e193a26185938
Hosam Hatem <hosam.hatem222@gmail.com>
2021-08-20 06:29:04 +0200

    solution of problem

---
+++ 2018 IME Tryouts/H. Heaven's Arena.cpp
@@ -0,0 +1,23 @@
+// link :-  https://codeforces.com/gym/101858/problem/H
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
+#include <bits/stdc++.h>
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf 0x3f3f3f3f3f3f3f3f
+#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define fo1(s, n) for(int i=s ; i<n ; i++)
+#define fo2(s) for(int i=s ; i>=0 ; i--)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int main()
+{
+    io;
+    double n, a, p;
+    cin >> n >> a >> p;
+    cout << fixed << setprecision(9) << (n + ((p/100) * a) - (((100-p)/100) * a));
+    return 0;
+}
