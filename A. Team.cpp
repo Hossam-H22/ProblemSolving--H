03b8c1e5ec02c437099e454788728637df86a11e
Hosam Hatem <88390970+Dragon-H22@users.noreply.github.com>
2021-09-30 12:41:44 +0200

    solution of problem

--- A. Team.cpp
+++ A. Team.cpp
@@ -3,27 +3,18 @@
 // |    -->  Dragon_H22  <--     |
 // ------------------------------- 
 #include <bits/stdc++.h>
-#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define inf 0x3f3f3f3f3f3f3f3f
-#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-#define fo1(s, n) for(int i=s ; i<n ; i++)
-#define fo2(s, n) for(int i=s ; i<=n ; i++)
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
 
 int main()
 {
-    io;
     int n, cont=0;
     cin>>n;
-    fo1(0, n)
+    for(int i=0 ; i<n ; i++)
     {
         int a,b,c;
         cin>>a>>b>>c;
         a +=(b+c);
-        if(a>=2) cont++;
+        if (a>=2) cont++;
     }
     cout << cont;
 
