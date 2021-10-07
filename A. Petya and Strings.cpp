232a33ed04e7a43e6946d37c8b013da55ffcc270
Hosam Hatem <88390970+Dragon-H22@users.noreply.github.com>
2021-10-07 11:33:58 +0200

    solution of problem

--- A. Petya and Strings.cpp
+++ A. Petya and Strings.cpp
@@ -14,7 +14,7 @@ int main()
     transform(s1.begin(),s1.end(),s1.begin(),::toupper);  
     transform(s2.begin(),s2.end(),s2.begin(),::toupper);
     
-    for(int i=0 ; s1.size() ; i++)
+    for(int i=0 ; i<s1.size() ; i++)
     {
         if(s1[i] > s2[i])
         {
