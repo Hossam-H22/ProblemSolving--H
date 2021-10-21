2802b4267f5b9b8611475c9a6d38c2b24dd5682d
Hosam Hatem <hosam.hatem222@gmail.com>
2021-10-21 12:17:22 +0200

    solution of problem

---
+++ A. Pangram.cpp
@@ -0,0 +1,31 @@
+// link :- https://codeforces.com/contest/520/problem/A
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    int arr[200]={};
+    cin >> n;
+    for (int i=0 ; i<n ; i++)
+    {
+        char ch;
+        cin >> ch;
+        if (ch>='A' && ch <='Z') ch+=32;
+        arr[ch]++;
+    }
+    for (int i=97 ; i<=122 ; i++)
+    {
+        if (arr[i]==0)
+        {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+
+    return 0;
+}
