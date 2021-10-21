f9551d6de7964da7c3ea4b482e4eb296a7b92133
Hosam Hatem <88390970+Dragon-H22@users.noreply.github.com>
2021-10-21 17:09:17 +0200

    solution of problem

--- A. Black Square.cpp
+++ A. Black Square.cpp
@@ -2,27 +2,18 @@
 // -------------------------------
 // |    -->  Dragon_H22  <--     |
 // -------------------------------
+
 #include <bits/stdc++.h>
-#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define inf 0x3f3f3f3f3f3f3f3f
-#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-#define fo1(s, n) for(int i=s ; i<n ; i++)
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
 
 int main()
 {
-    io;
     int arr[5]={}, cont=0;
-    fo1(1, 5) cin>>arr[i];
+    for (int i=1; i<5 ; i++) cin>>arr[i];
     string s;
     cin>>s;
-    fo1(0, s.size()) cont += arr[s[i]-'0'];
+    for (int i=0 ; i<s.size() ; i++) cont += arr[s[i]-'0'];
     cout << cont;
-
-
 
     return 0;
 }
