8247520c90f734aff4222451dcbbddf1d1671227
Hosam Hatem <hosam.hatem222@gmail.com>
2021-10-22 11:44:25 +0200

    solution of problem

---
+++ A. Fox And Snake.cpp
@@ -0,0 +1,20 @@
+// link :- https://codeforces.com/contest/510/problem/A
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    for (int i=0 ; i<n ; i++)
+    {
+        if (i%2==0) cout << string(m, '#') << "\n";
+        else if (i%4==1) cout << string(m-1, '.') << "#\n";
+        else cout << "#" << string(m-1, '.') << "\n";
+    }
+
+    return 0;
+}
