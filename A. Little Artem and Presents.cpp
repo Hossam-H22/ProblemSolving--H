d5254073e2f73aeb7f15d761e2145811b06fa9e5
Hosam Hatem <hosam.hatem222@gmail.com>
2021-10-30 20:38:55 +0200

    solution of problem

---
+++ A. Little Artem and Presents.cpp
@@ -0,0 +1,23 @@
+// link :- https://codeforces.com/contest/669/problem/A
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    if(n==0) cout<<0;
+    else if(n<=2) cout<<1;
+    else
+    {
+        long long ans=(n/3)*2;
+        if(n%3==0) cout<<ans;
+        else cout<<ans+1;
+    }
+
+
+    return 0;
+}
