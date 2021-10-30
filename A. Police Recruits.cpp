ec36e3074040cd4cd653153ee9ce1f3a05cfeb30
Hosam Hatem <88390970+Dragon-H22@users.noreply.github.com>
2021-10-30 18:21:09 +0200

    solution of problem

--- A. Police Recruits.cpp
+++ A. Police Recruits.cpp
@@ -1,22 +1,15 @@
 // link :-  https://codeforces.com/contest/427/problem/A
-
-//     -->  Dragon_H22  <--
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
 #include <bits/stdc++.h>
-#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define inf 0x3f3f3f3f3f3f3f3f
-#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-#define for(s, n) for(int i=s ; i<n ; i++)
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
 
 int main()
 {
-    io;
     int n, cont=0, x, free=0;
     cin>>n;
-    for(0, n)
+    for(int i=0 ; i<n ; i++)
     {
         cin >> x;
         if (x>0) free+=x;
