4c22c5e7190df56bd55256783163c3f434e7db1a
Hosam Hatem <hosam.hatem222@gmail.com>
2022-07-29 14:44:25 +0200

    1600 / C++

---
+++ C. Joty and Chocolate.cpp
@@ -0,0 +1,27 @@
+// link :- https://codeforces.com/contest/678/problem/C
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
+#include <bits/stdc++.h>
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf 0x3f3f3f3f3f3f3f3f
+#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define fo1(s, n) for(int i=s ; i<n ; i++)
+#define fo2(s, n) for(int i=s ; i>=n ; i--)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+
+int main()
+{
+    io;
+    ll n, a, b, p, q, comon;
+    cin>>n>>a>>b>>p>>q;
+    comon = n/ lcm(a,b);
+    cout<<(p*((n/a)-comon))+(q*((n/b)-comon))+(comon* max(p,q));
+
+
+    return 0;
+}
