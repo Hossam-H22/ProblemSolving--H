169789c01afe4ff8c9cf17eeaf1a78ee1290b3e2
Hosam Hatem <hosam.hatem222@gmail.com>
2022-07-29 14:11:12 +0200

    800

---
+++ A. George and Accommodation.cpp
@@ -0,0 +1,23 @@
+// link :- https://codeforces.com/contest/467/problem/A
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int t, cont=0;
+    cin>>t;
+    while (t--)
+    {
+        int p, q;
+        cin>>p>>q;
+        if(q-p>=2) cont++;
+    }
+    cout<<cont;
+
+
+    return 0;
+}
