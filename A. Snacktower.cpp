7ea2808ba41bc2779bfe3991f26ea94321e3cf13
Hosam Hatem <hosam.hatem222@gmail.com>
2022-07-29 14:18:35 +0200

    1100

---
+++ A. Snacktower.cpp
@@ -0,0 +1,28 @@
+// link :- https://codeforces.com/contest/767/problem/A
+// -------------------------------
+// |    -->  Dragon_H22  <--     |
+// -------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,arr[100009];
+    cin>>n;
+    int z=n;
+    for (int i=0; i<n; i++)
+    {
+        int num;
+        cin>>num;
+        arr[num]=1;
+        while(arr[z]==1)
+        {
+            cout<<z<<" ";
+            z--;
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
