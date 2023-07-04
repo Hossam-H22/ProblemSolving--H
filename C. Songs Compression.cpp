9044570de9be659bbed9e342f546c8f5b645f7f3
Hossam Hatem <hosam.hatem222@gmail.com>
2023-07-04 09:01:10 +0300

    1100/C++

---
+++ C. Songs Compression.cpp
@@ -0,0 +1,40 @@
+ link - httpscodeforces.comproblemsetproblem1015C
+ -------------------------------
+     --  Dragon_H22  --     
+ -------------------------------
+#include bitsstdc++.h
+#include iostream
+#define io ios_basesync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf 0x3f3f3f3f3f3f3f3f
+#define file freopen(input.txt,r,stdin);freopen(output.txt,w,stdout);
+#define fo1(s, n) for(int i=s ; in ; i++)
+#define fo2(s, n) for(int i=s ; i=n ; i--)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+
+int main()
+{
+    io;
+    ll n, m, b_sum=0, i=0, c=0, a, b;
+    cinnm;
+    c=n;
+    ll arr[n+5];
+    fo1(0, n) {
+        cinab;
+        arr[i] = a-b;
+        b_sum+=b;
+    }
+    if(b_summ) {
+        cout-1;
+        return 0;
+    }
+    sort(arr, arr+n);
+    while(b_sum=m && in) b_sum+=arr[i], i++, c--;
+
+    (b_sum=m) coutc  coutc+1;
+
+    return 0;
+}
