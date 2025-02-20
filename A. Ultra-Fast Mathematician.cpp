530098e5fefe596dbdf200e14746d6897bda7a0f
Hossam-H22 <hosam.hatem222@gmail.com>
2025-02-20 11:17:27 +0200

    800 / C++

---
+++ A. Ultra-Fast Mathematician.cpp
@@ -0,0 +1,37 @@
+// https://codeforces.com/contest/61/problem/A
+/*
+██████  ██████   █████   ██████   ██████  ███    ██         ██   ██ ██████  ██████
+██   ██ ██   ██ ██   ██ ██       ██    ██ ████   ██         ██   ██      ██      ██
+██   ██ ██████  ███████ ██   ███ ██    ██ ██ ██  ██         ███████  █████   █████
+██   ██ ██   ██ ██   ██ ██    ██ ██    ██ ██  ██ ██         ██   ██ ██      ██
+██████  ██   ██ ██   ██  ██████   ██████  ██   ████ ███████ ██   ██ ███████ ███████
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf 0x3f3f3f3f3f3f3f3f
+#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+
+void solve(){
+    string s1, s2, res="";
+    cin>>s1>>s2;
+    for(int i=0; i<s1.length(); i++) res+= (s1[i]==s2[i])? '0': '1';
+    cout<<res;
+}
+
+
+int main()
+{
+    io;
+    int t=1;
+//    cin>>t;
+    while(t--) solve();
+
+    return 0;
+}
