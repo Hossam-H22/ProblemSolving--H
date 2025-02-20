ba045fb620355466f45879817565daf6064da3f6
Hossam-H22 <hosam.hatem222@gmail.com>
2025-02-20 12:47:32 +0200

    800 / C++

---
+++ A. Buy a Shovel.cpp
@@ -0,0 +1,41 @@
+// https://codeforces.com/problemset/problem/732/A
+/*
+██████  ██████   █████   ██████   ██████  ███    ██         ██   ██ ██████  ██████
+██   ██ ██   ██ ██   ██ ██       ██    ██ ████   ██         ██   ██      ██      ██
+██   ██ ██████  ███████ ██   ███ ██    ██ ██ ██  ██         ███████  █████   █████
+██   ██ ██   ██ ██   ██ ██    ██ ██    ██ ██  ██ ██         ██   ██ ██      ██
+██████  ██   ██ ██   ██  ██████   ██████  ██   ████ ███████ ██   ██ ███████ ███████
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf 0x3f3f3f3f3f3f3f3f
+#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+
+void solve(){
+    int k, r;
+    cin>>k>>r;
+    for(int i=1; ; i++){
+        if( (k*i)%10==r || (k*i)%10== 0){
+            cout<<i;
+            break;
+        }
+    }
+}
+
+
+int main()
+{
+    io;
+    int t=1;
+//    cin>>t;
+    while(t--) solve();
+
+    return 0;
+}
