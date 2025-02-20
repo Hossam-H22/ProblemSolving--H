6a9b80969091a498ac0c2a97edecf6b800320c24
Hossam-H22 <hosam.hatem222@gmail.com>
2025-02-20 10:49:47 +0200

    800 / C++

---
+++ A. The New Year  Meeting Friends.cpp
@@ -0,0 +1,35 @@
+// https://codeforces.com/contest/723/problem/A
+/*
+██████  ██████   █████   ██████   ██████  ███    ██         ██   ██ ██████  ██████
+██   ██ ██   ██ ██   ██ ██       ██    ██ ████   ██         ██   ██      ██      ██
+██   ██ ██████  ███████ ██   ███ ██    ██ ██ ██  ██         ███████  █████   █████
+██   ██ ██   ██ ██   ██ ██    ██ ██    ██ ██  ██ ██         ██   ██ ██      ██
+██████  ██   ██ ██   ██  ██████   ██████  ██   ████ ███████ ██   ██ ███████ ███████
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define inf 0x3f3f3f3f3f3f3f3f
+#define file freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+void solve(){
+    int x1,x2,x3;
+    cin>>x1>>x2>>x3;
+    cout<<abs(max(x1,max(x2, x3)) - min(x1, min(x2, x3)))<<endl;
+}
+
+
+int main()
+{
+    io;
+    int t=1;
+//    cin>>t;
+    while(t--) solve();
+
+    return 0;
+}
